e1e42e575468ac0c0ec112aa85a804bc59cff3bd
janier2000 <jomr10@hotmail.com>
2026-04-09 10:37:58 -0500

    Events in the life cycle of a web application Part 4 https://www.youtube.com/watch?v=BmshuDDOcuA&list=PL6n9fhu94yhXQS_p1i-HLIftB9Y7Vnxlo&index=4

---
+++ WebAppASP.net/WebAppASP.net/Part4/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppASP.net.Part4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Number of Applications: " + Application["TotalApplications"]);
+            Response.Write("<br/>");
+            Response.Write("Number of Users Online: " + Application["TotalUserSessions"]);
+        }
+    }
+}
