cfff8ab59bbc9419fa3b618a6e29caf1ab034665
janier2000 <jomr10@hotmail.com>
2026-04-17 10:45:30 -0500

    17-04-2026

---
+++ WebAppASP.net/WebAppASP.net/7-Eventos de control del servidor/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebAppASP.net.P7_Eventos_de_control_del_servidor.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="campo texto obligatorio (RegularExpressionValidator)"></asp:RegularExpressionValidator>
+    </form>
+</body>
+</html>
---
+++ WebAppASP.net/WebAppASP.net/7-Eventos de control del servidor/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppASP.net.P7_Eventos_de_control_del_servidor
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Response.Write("1 - Page_PreInit" + "<br/>");
+        }
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Write("2 -Page_Init" + "<br/>");
+        }
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            Response.Write("3 - Page_InitComplete" + "<br/>");
+        }
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            Response.Write("4 - Page_PreLoad" + "<br/>");
+        }
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            Response.Write("5 - Page_LoadComplete" + "<br/>");
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            Response.Write("6 - Page_PreRender" + "<br/>");
+        }
+        protected void Page_PreRenderComplete(object sender, EventArgs e)
+        {
+            Response.Write("7 - Page_PreRenderComplete" + "<br/>");
+        }
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Response.Write("Text Changed Event TextBox1" + "<br/>");
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("Button Click" + "<br/>");
+        }
+
+       
+    }
+}
