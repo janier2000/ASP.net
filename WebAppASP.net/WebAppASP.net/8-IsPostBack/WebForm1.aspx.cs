b080ba38355da84babe8bb6af7b4f00fd330674b
janier2000 <jomr10@hotmail.com>
2026-04-17 11:08:56 -0500

    IsPostBack in asp.net - Part 8

---
+++ WebAppASP.net/WebAppASP.net/8-IsPostBack/WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppASP.net._8_IsPostBack_in_asp.net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //se tubiera eu utilizar este sino utiliza el postback
+            //ddlCity.Items.Clear();
+
+            if (!Page.IsPostBack)
+            {
+                Response.Write("Page_Load cargada por primera vez" + "<br/>");
+                LoadCityDropDownList();
+            }
+            else
+            {
+                Response.Write("Page_Load entrado despues de la primera vez" + "<br/>");
+            }
+            //
+        }
+        public void LoadCityDropDownList()
+        {
+            ListItem li1 = new ListItem("London");
+            ddlCity.Items.Add(li1);
+
+            ListItem li2 = new ListItem("Sydney");
+            ddlCity.Items.Add(li2);
+
+            ListItem li3 = new ListItem("Mumbai");
+            ddlCity.Items.Add(li3);
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
