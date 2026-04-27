f2b56412b9d52602b3311375eb829a272eac6acd
janier2000 <jomr10@hotmail.com>
2026-04-27 10:24:03 -0500

    l Part 12 CheckBox Contro - https://www.youtube.com/watch?v=jyghYnwXvKI&list=PL6n9fhu94yhXQS_p1i-HLIftB9Y7Vnxlo&index=12

---
+++ WebAppASP.net/WebAppASP.net/12 - CheckBox Control/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebAppASP.net._12___CheckBox_Control.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: Arial">
+            <fieldset style="width: 350px">
+                <legend><b>Education</b></legend>
+                <asp:CheckBox ID="GraduateCheckBox"  Text="Graduate" runat="server" AutoPostBack="True" OnCheckedChanged="GraduateCheckBox_CheckedChanged" />
+                <asp:CheckBox ID="PostGraduateCheckBox" Checked="true" Text="Post Graduate" runat="server" />
+                <asp:CheckBox ID="DoctrateCheckBox" Text="Doctrate" runat="server" />
+            </fieldset>
+            &nbsp;
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAppASP.net/WebAppASP.net/12 - CheckBox Control/WebForm1.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebAppASP.net._12___CheckBox_Control {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control GraduateCheckBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox GraduateCheckBox;
+        
+        /// <summary>
+        /// Control PostGraduateCheckBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox PostGraduateCheckBox;
+        
+        /// <summary>
+        /// Control DoctrateCheckBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox DoctrateCheckBox;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
