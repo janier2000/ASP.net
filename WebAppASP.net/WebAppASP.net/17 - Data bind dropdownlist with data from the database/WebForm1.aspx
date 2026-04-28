2cca0361cd93138609f173b182e11bb17a284f34
janier2000 <jomr10@hotmail.com>
2026-04-28 10:40:29 -0500

    Data bind asp.net dropdownlist with data from the database https://www.youtube.com/watch?v=wf9WceS1lFo&list=PL6n9fhu94yhXQS_p1i-HLIftB9Y7Vnxlo&index=17

---
+++ WebAppASP.net/WebAppASP.net/17 - Data bind dropdownlist with data from the database/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebAppASP.net._17___Data_bind_asp.net_dropdownlist_with_data_from_the_database.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" DataTextField="CityName"
+                DataValueField="CityId" runat="server">
+            </asp:DropDownList>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ WebAppASP.net/WebAppASP.net/17 - Data bind dropdownlist with data from the database/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppASP.net._17___Data_bind_asp.net_dropdownlist_with_data_from_the_database
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlCommand cmd = new SqlCommand("Select CityId, CityName, Country from tblCity", con);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    DropDownList1.DataTextField = "CityName";
+                    DropDownList1.DataValueField = "CityId";
+                    DropDownList1.DataSource = rdr;
+                    DropDownList1.DataBind();
+                }
+            }
+        }
+    }
+}
