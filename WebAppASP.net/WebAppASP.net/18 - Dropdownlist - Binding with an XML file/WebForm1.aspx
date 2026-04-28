af6d5e76560db375bf064ddc056d6adfd36c914d
janier2000 <jomr10@hotmail.com>
2026-04-28 10:58:30 -0500

    parte 18 Binding an asp.net dropdownlist with an XML file https://www.youtube.com/watch?v=StgoYI_B7ks&list=PL6n9fhu94yhXQS_p1i-HLIftB9Y7Vnxlo&index=18

---
+++ WebAppASP.net/WebAppASP.net/18 - Dropdownlist - Binding with an XML file/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebAppASP.net._18___Binding_an_dropdownlist_with_an_XML_file.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:dropdownlist ID="DropDownList1" runat="server"></asp:dropdownlist>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAppASP.net/WebAppASP.net/18 - Dropdownlist - Binding with an XML file/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppASP.net._18___Binding_an_dropdownlist_with_an_XML_file
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Create a new DataSet
+            DataSet DS = new DataSet();
+            //Read the xml data from the XML file using ReadXml() method
+
+            string rutafisica = Server.MapPath("./Countries.xml");
+
+            DS.ReadXml(Server.MapPath("./Countries.xml")); //Server.MapPath devuelve la ruta fisica del archivo
+            DropDownList1.DataTextField = "CountryName";
+            DropDownList1.DataValueField = "CountryId";
+            DropDownList1.DataSource = DS;
+            DropDownList1.DataBind();
+            ListItem li = new ListItem("Select", "-1");
+            DropDownList1.Items.Insert(0, li);
+        }
+    }
+}
